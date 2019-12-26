9d6139aa18c32afee1a93ffcd9b467732e052034
bhertel <brendan_hertel@student.uml.edu>
2019-12-26 15:56:40 -0500

    12/26/19

--- src/brendan_ur5e/src/scripts/read_hdf5.py
+++ src/brendan_ur5e/src/scripts/read_hdf5.py
@@ -7,7 +7,7 @@ import numpy as np
 
 if __name__ == '__main__':
 	rospy.init_node('read_hdf5')
-	hf = h5py.File('recorded_demo Tue Nov 26 18:52:31 2019.h5', 'r')
+	hf = h5py.File('recorded_demo Thu Dec 26 14:14:19 2019.h5', 'r')
 	keys = hf.keys()
 	print(keys)
 	demo = hf.get('demo1')
@@ -16,9 +16,18 @@ if __name__ == '__main__':
 	joint_state_info = demo.get('joint_state_info')
 	joint_state_items = joint_state_info.items()
 	print(joint_state_items)
+	tf_info = demo.get('tf_info')
 	joint_pos_info = joint_state_info.get('joint_positions')
 	joint_pos_info = np.array(joint_pos_info)
-	for i in range(0, np.size(joint_pos_info, 1)):
-		print(joint_pos_info[0][i])
-		print(joint_pos_info[1][i])
-		print(joint_pos_info[2][i])
+	pos_rot_data = tf_info.get('pos_rot_data')
+	pos_rot_data = np.array(pos_rot_data)
+	hf.close()
+	for i in range(0, np.size(pos_rot_data, 1)):
+		print("translation x: ", pos_rot_data[0][i])
+		print("translation y: ", pos_rot_data[1][i])
+		print("translation z: ", pos_rot_data[2][i])
+		print("rotation x: ", pos_rot_data[3][i])
+		print("rotation y: ", pos_rot_data[4][i])
+		print("rotation z: ", pos_rot_data[5][i])
+		print("rotation w: ", pos_rot_data[6][i])
+		print
